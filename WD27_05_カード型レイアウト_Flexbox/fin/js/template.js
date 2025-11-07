d8f3e53ffd1f51023a1f04727861c128325221f4
ovdesign <koizumi@ovdesign.jp>
2025-11-07 10:51:08 +0900

    Add files via upload

--- WD27_05_カード型レイアウト_Flexbox/fin/js/template.js
+++ WD27_05_カード型レイアウト_Flexbox/fin/js/template.js
@@ -2,6 +2,7 @@
 //ナビゲーション・コピーライト一括管理
 ////////////////////////////////////////
 
+// Vue JS パターン///////////////////////////////
 //ナビゲーション部分
 Vue.component("navigation-template", {
   template: `
@@ -26,10 +27,33 @@ new Vue({
 //コピーライト部分
 Vue.component("copyright-template", {
   template: `
-  <small>&copy; 2024 十勝豆知識</small>
+  <small>&copy; 2025 十勝豆知識</small>
   `,
 });
 
 new Vue({
   el: "#copyright",
 });
+
+// バニラ JS パターン///////////////////////////////
+// ナビゲーション部分
+// const navHTML = `
+//   <ul>
+//     <li><a href="index.html">HOME</a></li>
+//     <li><a href="azuki.html">豆のゆで方</a></li>
+//     <li><a href="azuki.html">豆の力</a></li>
+//     <li><a href="azuki.html">世界の豆</a></li>
+//     <li><a href="azuki.html">このサイトについて</a></li>
+//   </ul>
+// `;
+
+// // 対象の要素に挿入
+// document.addEventListener("DOMContentLoaded", () => {
+//   const mainNav = document.getElementById("main-navigation");
+//   const footerNav = document.getElementById("footer-navigation");
+//   const copyright = document.getElementById("copyright");
+
+//   if (mainNav) mainNav.innerHTML = navHTML;
+//   if (footerNav) footerNav.innerHTML = navHTML;
+//   if (copyright) copyright.innerHTML = `<small>&copy; 2025 十勝豆知識</small>`;
+// });
